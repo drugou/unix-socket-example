de67a7b0de8f13202a383cf86994b426e7a06678
Pavel Adamanov <adamanov.pavel@lenvendo.ru>
2020-02-25 17:51:45 +0300

    Добавил структуру

---
+++ app/Application.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App;
+
+use Dotenv;
+
+/**
+ * Class Application
+ *
+ * Класс приложения.
+ *
+ * @package App
+ */
+class Application
+{
+    /**
+     * Возвращает корневую директорию приложения
+     * @return string корневая директория приложения
+     */
+    public function getRootDir(): string
+    {
+        return dirname(__DIR__);
+    }
+
+    /**
+     * Загружает приложение
+     */
+    public function boot(): void
+    {
+        $this->loadEnv();
+    }
+
+    /**
+     * Загружает ENV файл
+     */
+    protected function loadEnv(): void
+    {
+        $directory = $this->getRootDir();
+        $dotenv = Dotenv\Dotenv::create($directory);
+        $dotenv->load();
+    }
+}
